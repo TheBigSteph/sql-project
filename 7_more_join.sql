601d1b68ff493640a707f1b1f1951f3c75102e5a
thebigsteph <nzali.stephane86@gmail.com>
2019-02-22 13:00:54 +0100

    completed the queries of more_join section

--- 7_more_join.sql
+++ 7_more_join.sql
@@ -17,3 +17,27 @@ SELECT title FROM movie m JOIN casting c WHERE  c.movieid = m.id AND c.actorid =
 SELECT title FROM movie m JOIN casting c WHERE  c.movieid = m.id AND c.actorid = (SELECT id FROM actor WHERE name = 'Harrison Ford') AND ord != 1
 
 SELECT title, name FROM movie JOIN casting ON movie.id = movieid JOIN actor ON actorid = actor.id WHERE yr = 1962 AND ord = 1;
+
+SELECT yr,COUNT(title) FROM
+  movie JOIN casting ON movie.id=movieid
+         JOIN actor   ON actorid=actor.id
+where name='John Travolta'
+GROUP BY yr
+HAVING COUNT(title)=(SELECT MAX(c) FROM
+(SELECT yr,COUNT(title) AS c FROM
+   movie JOIN casting ON movie.id=movieid
+         JOIN actor   ON actorid=actor.id
+ where name='John Travolta'
+ GROUP BY yr) AS t
+)
+
+SELECT movie.title, actor.name FROM movie JOIN casting ON (movie.id = casting.movieid AND ord = 1) JOIN actor ON actor.id = casting.actorid
+WHERE movie.id IN (SELECT movieid FROM casting WHERE actorid IN (SELECT id FROM actor WHERE name = 'Julie Andrews'))
+
+SELECT a.name FROM casting c JOIN actor a ON a.id = c.actorid AND ord = 1 GROUP BY a.name HAVING COUNT(a.name) >= 30
+
+
+SELECT m.title, COUNT(c.actorid) FROM movie m JOIN casting c ON m.id = c.movieid WHERE m.yr = 1978 GROUP BY m.title, m.yr ORDER BY COUNT(c.actorid) DESC, m.title
+
+SELECT a.name FROM casting c JOIN actor a ON c.actorid = a.id WHERE c.movieid IN (SELECT m.id FROM movie m JOIN casting c ON m.id = c.movieid JOIN actor a ON c.actorid = a.id
+WHERE a.name = 'Art Garfunkel' GROUP BY m.id) AND a.name != 'Art Garfunkel'
